5f4e7cd6234cbcc46a71c25f6b61dcb9a3ff4710
QueenEzinne <zynniequeen1@gmail.com>
2023-06-20 07:51:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - print _putchar
- * is written by mbah
+ * is written by Ezinne
  * Return: 0
  */
 
@@ -17,4 +17,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-}	
+}
