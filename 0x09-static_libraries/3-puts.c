5aab4c5fbbb0cd63fa7853fb38507a03677ae26b
QueenEzinne <zynniequeen1@gmail.com>
2023-07-07 16:42:44 -0700

    static_libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -3,10 +3,11 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
+ * _putchar prints a new line
  */
 void _puts(char *str)
 {
-while (*str != '\0')
+while (*str)
 {
 _putchar(*str++);
 }
